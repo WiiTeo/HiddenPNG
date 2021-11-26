dc4e036a400e5355373fd7a0e48a7dcce28760eb
Téo <93946963+WiiTeo@users.noreply.github.com>
2021-11-26 21:35:25 +0100

    Add files via upload

---
+++ HiddenPNG.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from tkinter.messagebox import *
+from tkinter.filedialog import *
+import os
+import sys
+
+filepath = askopenfilename(title="Ouvrir une image",filetypes=[('PNG Image File','.png'),('Other File','.*')])
+
+fileopen = open(filepath, "a")
+
+print("HiddenPNG 2021 (by WiiTeo)")
+print(f"Selected image : {filepath}")
+
+userType = input("Text>")
+
+fileopen.write(userType)
+fileopen.close()
